d3cc349d4153133bf58b94fc21f05c0bd35d8920
Andrei Timofeev <97550457+FootballOnlooker@users.noreply.github.com>
2026-07-06 11:46:52 +0200

    Update main.py

--- teil1/main.py
+++ teil1/main.py
@@ -1,15 +1,16 @@
+#Ich importiere die Bibliothek ollama, damit mein Python-Programm mit dem lokalen KI-Modell kommunizieren kann.
 import ollama
 
-
-def ask_ai(prompt):
+# Send a user message to the local Ollama model and return the AI response.
     response = ollama.chat(
-        model="llama3.2:3b",
+        model="llama3.2:3b", #wie heißt model Ollama
         messages=[
             {
-                "role": "user",
+                "role": "user", #Die Rolle ist „user“, weil diese Nachricht vom Benutzer kommt.
                 "content": prompt
             }
         ]
     )
 
-    return response["message"]["content"]
+    # Return only the generated text from the response.
+    return response["message"]["content"] 
